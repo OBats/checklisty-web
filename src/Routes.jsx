1ab3a763d6f78578cf47c471d81227816b8272c4
Taras <>
2019-01-30 14:45:49 +0200

    Closes - #1 Authentication routing : Sign In/ Sign Up (front end side)

---
+++ src/Routes.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
+// import { Redirect } from 'react-router';
+
+
+const Routes = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" component={} />
+		</Switch>
+	</BrowserRouter>
+)
+	
+
+export default Routes;
