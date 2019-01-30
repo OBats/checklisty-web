5ad786379f635211f73151a848d323fdf7d2c429
Roman Lytvyn <36104838+romanolytvyn@users.noreply.github.com>
2019-01-30 16:49:54 +0200

    Closes #1 - Authentication routing : Sign In/ Sign Up (front end side) (#9)

---
+++ src/Routes.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+// import { Redirect } from 'react-router';
+
+
+const Routes = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route path="/auth/signin/" component={SignIn} />
+			<Route path="/auth/signup/" component={SignUp} />
+		</Switch>
+	</BrowserRouter>
+)
+	
+
+export default Routes;
---
+++ src/components/auth/SignIn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import {Switch, BrowserRouter, Route} from 'react-router-dom';
+
+
+class SignIn extends Component {
+  render() {
+    return (
+		<div> SignIn </div>
+    )
+  }
+}
+
+export default SignIn;
