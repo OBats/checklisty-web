fd5f14cb969b106c348a93a9b2fe22335b9efcc0
Roman Kordyuk <kordukroman@gmail.com>
2019-01-31 12:34:08 +0200

    Closes #4 - Add pre-commit hook

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 // import {Switch, BrowserRouter, Route} from 'react-router-dom';
 
-
 class SignIn extends Component {
-  render() {
-    return (
-		<div>SignIn</div>
-    );
-  }
+	render() {
+		return <div>SignIn</div>;
+	}
 }
 
 export default SignIn;
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 // import {Switch, BrowserRouter, Route} from 'react-router-dom';
 
-
 class SignUp extends Component {
-  render() {
-    return (
-		<div>SignUp</div>
-    )
-  }
+	render() {
+		return <div>SignUp</div>;
+	}
 }
 
 export default SignUp;
