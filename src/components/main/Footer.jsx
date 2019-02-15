6243d1a0e25f609bc8daaa2adc9301834fef06e5
Yuri Pristay <31614851+Yuri-Pristay@users.noreply.github.com>
2019-02-15 17:22:28 +0200

    Closes #51 - Add information to footer (#78)

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => (
-  <footer className={styles.block}>
-    <b>Footer</b>
+  <footer className={styles.menu}>
+    <span className={styles.fonts}>Made with ❤️ by Lv-379 NodeJS</span>
+    <a href="https://github.com/ss-ita/checklisty-web">
+      <img
+        className={styles.distance}
+        src="https://camo.githubusercontent.com/05034eed0515ee94cb8c8829dad9ace1f4e7d79e/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f46726f6e74254532253830253931456e645f436865636b6c6973742d666f6c6c6f7765642d627269676874677265656e2e737667"
+        alt="Front‑End_Checklisty followed"
+        data-canonical-src="https://img.shields.io/badge/Front%E2%80%91End_Checklist-followed-brightgreen.svg"
+      />
+    </a>
   </footer>
 );
 
