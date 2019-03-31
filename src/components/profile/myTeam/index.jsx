47357104708a21af7d593cd4abbc33b192862173
Rostyk Machikha <34648058+machikharostyk@users.noreply.github.com>
2019-03-31 18:02:30 +0300

    Resolve problem when return from team (#236)

--- src/components/profile/myTeam/index.jsx
+++ src/components/profile/myTeam/index.jsx
@@ -30,6 +30,7 @@ const MyTeam = (props) => {
 
   useEffect(() => () => {
     changeComponentLoading(true);
+    fetchUserTeams(activePage, searchTeamValue, selectTeams);
   }, []);
 
   useEffect(() => {
