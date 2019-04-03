0fe40bbcd5c04e31209e7a6ddf542e0232885729
Anzhelika Semeniuk <Anzhelika@maxs-mbp.training.local>
2019-04-03 11:43:03 +0300

    Fix a bug in ChatSideBar component

--- src/components/toasters/MessagesHandling.jsx
+++ src/components/toasters/MessagesHandling.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+let toastId;
 
-export const ErrorHandling = msg => toast.error(msg, {
-  position: 'bottom-center',
-  autoClose: 4000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-});
+export const ErrorHandling = (msg, timeout = 2000) => {
+  if (!toast.isActive(toastId)) {
+    toastId = toast.error(msg, {
+      toastId,
+      position: 'bottom-center',
+      autoClose: timeout,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      preventDuplicates: true,
+    });
+  }
+};
 
 export const SuccessHandling = msg => toast.success(msg, {
   position: 'bottom-center',
@@ -40,6 +47,7 @@ export const MessageContainer = () => (
     rtl={false}
     pauseOnVisibilityChange
     draggable
+    preventDuplicates
     transition={Zoom}
   />
 );
